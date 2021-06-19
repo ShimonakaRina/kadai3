864e7d0db3d1adf61563e21fcbf4252768f28b25
ShimonakaRina <sixseasonrain11614@gmail.com>
2021-06-19 14:00:31 +0000

    bookedit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,8 @@
 class BooksController < ApplicationController
   def index
+    @user = current_user
+    @books = Book.all
+    @book = Book.new
   end
 
   def new
@@ -8,12 +11,30 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
+    book.user_id = current_user.id
     book.save
-    redirect_to book_id_path
+    redirect_to book_path(book.id)
+  end
+
+  def show
+    @user = current_user
+    @books = Book.all
+    @book = Book.new
+    @book1 = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    @book1 = Book.find(params[:id])
+    @book1.destroy
+    redirect_to books_path
   end
 
   private
   def book_params
-    params.require(:book).permit(:title, :body, :image)
+    params.require(:book).permit(:title, :body)
   end
 end
