3c41d87376b5982151e0ec638afbbf3592afb53e
ShimonakaRina <sixseasonrain11614@gmail.com>
2021-07-15 01:51:07 +0000

    dm

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,7 @@ class UsersController < ApplicationController
     @book = Book.new
     @currentUserEntry=Entry.where(user_id: current_user.id)
     @userEntry=Entry.where(user_id: @user.id)
-    if @user.id == current_user.id
-    else
+    unless @user.id == current_user.id
       @currentUserEntry.each do |cu|
         @userEntry.each do |u|
           if cu.room_id == u.room_id then
